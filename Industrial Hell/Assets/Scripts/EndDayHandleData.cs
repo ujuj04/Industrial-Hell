126828150f49599fa2718a9831a2f6ca49e772b3
George Karev <george.karev.04@gmail.com>
2024-12-19 04:40:03 +0800

    sequences set up, only need to scale now

--- Industrial Hell/Assets/Scripts/EndDayHandleData.cs
+++ Industrial Hell/Assets/Scripts/EndDayHandleData.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -20,6 +21,17 @@ public class EndDayHandleData : MonoBehaviour
     public int foodWellBeingValue = 20;
     private bool isFoodActive = false;
 
+    [SerializeField] Button medicineDisabledButton;
+    [SerializeField] Button medicineEnabledButton;
+    public int medicinePriceValue = 40;
+    public int medicineWellBeingValue = 25;
+    private bool isMedicineActive = false;
+
+    private int wellBeingValueTemp = 0;
+
+    [SerializeField] GameObject popup;
+    private Coroutine coroutinePopup;
+
 
     private void Awake()
     {
@@ -39,7 +51,6 @@ public class EndDayHandleData : MonoBehaviour
         {
             wellBeingValue.text = variableManager.WellBeingValue.ToString();
             wellBeingMeter.fillAmount = variableManager.WellBeingValue / 100f;
-            Debug.Log(variableManager.WellBeingValue / 100f);
 
             energyGeneratedText.text = variableManager.EnergyCount.ToString();
 
@@ -62,24 +73,105 @@ public class EndDayHandleData : MonoBehaviour
         {
             if (variableManager.MoneyCount - foodPriceValue > 0)
             {
-                variableManager.WellBeingValue += foodWellBeingValue;
+                if (variableManager.WellBeingValue + foodWellBeingValue < 100)
+                {
+                    variableManager.WellBeingValue += foodWellBeingValue;
+                }
+                else
+                {
+                    wellBeingValueTemp = (variableManager.WellBeingValue + foodWellBeingValue) - 100;
+                    variableManager.WellBeingValue = 100;
+                }
+
                 variableManager.MoneyCount -= foodPriceValue;
                 foodEnabledButton.gameObject.SetActive(true);
                 isFoodActive = true;
             }
             else
             {
-                Debug.Log("not enough money");
+                CreatePopup("Not Enough $");
             }
         }
         else
         {
             variableManager.WellBeingValue -= foodWellBeingValue;
+            if (wellBeingValueTemp > 0)
+            {
+                variableManager.WellBeingValue += wellBeingValueTemp;
+                wellBeingValueTemp = 0;
+            }
+
             variableManager.MoneyCount += foodPriceValue;
             foodEnabledButton.gameObject.SetActive(false);
             isFoodActive = false;
         }
 
         UpdateUI();
+    }
+
+    public void BuyMedicine()
+    {
+        if (!isMedicineActive)
+        {
+            if (variableManager.MoneyCount - medicinePriceValue > 0)
+            {
+                if (variableManager.WellBeingValue + medicineWellBeingValue < 100)
+                {
+                    variableManager.WellBeingValue += medicineWellBeingValue;
+                }
+                else
+                {
+                    wellBeingValueTemp = (variableManager.WellBeingValue + medicineWellBeingValue) - 100;
+                    variableManager.WellBeingValue = 100;
+                }
+
+                variableManager.MoneyCount -= medicinePriceValue;
+                medicineEnabledButton.gameObject.SetActive(true);
+                isMedicineActive = true;
+            }
+            else
+            {
+                CreatePopup("Not Enough $");
+            }
+        }
+        else
+        {
+            variableManager.WellBeingValue -= medicineWellBeingValue;
+            if (wellBeingValueTemp > 0)
+            {
+                variableManager.WellBeingValue += wellBeingValueTemp;
+                wellBeingValueTemp = 0;
+            }
+
+            variableManager.MoneyCount += medicinePriceValue;
+            medicineEnabledButton.gameObject.SetActive(false);
+            isMedicineActive = false;
+        }
+
+        UpdateUI();
+    }
+
+
+
+
+    public void CreatePopup(string message)
+    {
+        if (coroutinePopup != null)
+        {
+            StopCoroutine(coroutinePopup);
+        }
+
+        coroutinePopup = StartCoroutine(ShowPopupCoroutine(message));
+    }
+
+    private IEnumerator ShowPopupCoroutine(string message)
+    {
+        popup.gameObject.SetActive(true);
+
+        // Wait for 3 seconds
+        yield return new WaitForSeconds(2);
+
+        popup.gameObject.SetActive(false);
+        coroutinePopup = null;
     }
 }
